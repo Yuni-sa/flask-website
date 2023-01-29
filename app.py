eb688c484f1e518ebb62c4562d406d63dc4f4a3d
Yonatan Sasson <yonatanxd7@gmail.com>
2023-01-30 00:42:11 +0200

    First commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/data")
+def data():
+    df = pd.read_excel("data.xlsx")
+    table = df.to_html()
+    return render_template("data.html", table=table)
