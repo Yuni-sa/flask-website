25a47134861c65e7fb803eeec6e220f127e397c7
Yonatan Sasson <yonatanxd7@gmail.com>
2023-01-30 22:17:53 +0200

    servers and phones pages

--- app.py
+++ app.py
@@ -8,11 +8,17 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route("/data")
-def data():
+@app.route("/servers")
+def servers():
     df = pd.read_excel("data.xlsx")
     table = df.to_html()
-    return render_template("data.html", table=table)
+    return render_template("Servers.html", table=table)
+
+@app.route("/phones")
+def phones():
+    df = pd.read_excel("phones.xlsx")
+    table = df.to_html()
+    return render_template("phones.html", table=table)
 
 if __name__ == '__main__':
     app.run(debug=True)
